5e7f14326fe977690017c43760c53850119f845a
Dmitry Polovinkin <dtpolovinkin@gmail.com>
2018-03-12 18:32:03 +0300

    First commit

---
+++ bycicles.py
@@ -0,0 +1,2 @@
+bycicles = ["Trek", "Scott", "Kross", "GT"]
+print("My favorite bycicle was made by " + bycicles[2] + " company.") 
---
+++ name_cases.py
@@ -0,0 +1,14 @@
+#1st
+name = "Timofey"
+message = "Hello " + name + ", what a wonderful day today, isn't it?"
+print(message)
+
+#2nd
+print(name.upper())
+print(name.lower())
+
+#3rd
+famous_name = 'Bobby Axelrod'
+famous_message = 'As ' + famous_name + (' once said: "What\'s the point in having a "Fuck you" ' 
+'amount of money, if you can\'t say "Fuck you"?"')
+print(famous_message)
