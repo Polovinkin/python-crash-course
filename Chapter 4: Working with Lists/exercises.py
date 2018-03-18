2fd262ba615ef4ffddd35843cd9c5f85248c22f9
Dmitry Polovinkin <dtpolovinkin@gmail.com>
2018-03-18 18:43:53 +0300

    4 chapter exercises

---
+++ Chapter 4: Working with Lists/exercises.py
@@ -0,0 +1,17 @@
+#Exercise 4-3: Counting to Twenty
+twenty = [foo for foo in range(0,21)]
+print(twenty)
+
+#Exercise 4-4: One Million - it's the same
+
+#Exercise 4-5: Summing a Million
+million = [bar for bar in range(0,1000001)]
+print(min(million))
+print(max(million))
+print(sum(million))
+
+#Ex.4-6: Odd Numbers
+odd_list =[odd for odd in range(1,20,2)]
+print(odd_list)
+
+#Ex.4-7: Threes
