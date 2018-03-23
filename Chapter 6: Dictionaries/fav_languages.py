d519f67abe2e454c020a9f3090faab5988310285
Dmitry Polovinkin <dtpolovinkin@gmail.com>
2018-03-23 11:59:39 +0300

    finishing dictionaries chapter soon

--- Chapter 6: Dictionaries/fav_languages.py
+++ Chapter 6: Dictionaries/fav_languages.py
@@ -1,10 +1,11 @@
 favourite_languages = {
-    'dmitry': 'python',
-    'grigory': 'c#',
-    'edward': 'c++',
-    'guido': 'python',
+    'dmitry': ['python', 'sql'],
+    'grigory': ['c#', 'python'],
+    'edward': ['c++', 'haskell'],
+    'guido': ['python', 'c'],
 }
 
-print('The following languages have been mentioned:')
-for language in set(favourite_languages.values()):
-    print(language.title())
+for name, languages in favourite_languages.items():
+    print('\n' + name.title() + "'s favourite languages are:")
+    for language in languages:
+        print('\t' + language.title())
