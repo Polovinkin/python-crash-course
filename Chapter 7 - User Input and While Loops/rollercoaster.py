497189611b78fa462ddcd8aee14271e5cb6fa6ed
Dmitry Polovinkin <dtpolovinkin@gmail.com>
2018-03-29 22:27:18 +0300

    Small change

--- Chapter 7 - User Input and While Loops/rollercoaster.py
+++ Chapter 7 - User Input and While Loops/rollercoaster.py
@@ -2,6 +2,6 @@ height = input('How tall are you, in inches? ')
 height = int(height)
 
 if height >= 36:
-    print('\nYou are tall enough, boy!')
+    print('\nYou are tall enough!')
 else:
     print('\nReturn when you get older, dog!')
