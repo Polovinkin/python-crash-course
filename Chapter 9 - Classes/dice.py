54777e6b1b4d59b52e9031d7592c3e7add045bcb
Dmitry <dtpolovinkin@gmail.com>
2018-04-30 03:29:21 +0300

    Nice dice exercise

---
+++ Chapter 9 - Classes/dice.py
@@ -0,0 +1,18 @@
+from random import randint
+
+class Die():
+    def __init__(self, sides):
+        self.sides = sides
+    
+    def roll_die(self):
+        number = randint(1, self.sides)
+        return(number)
+    
+dice = Die(20)
+results = []
+
+for i in range(10):
+    result = dice.roll_die()
+    results.append(result)
+    i += 1
+print(results)
